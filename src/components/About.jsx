1c182a4091158839f325d9020ae1fa023b425e65
narek.primyan <narek.primyan@codetime.am>
2024-10-14 15:15:45 +0400

    added  3D(PC) model finished development of Hero Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Hi there! I’m Narek Primyan, a Software Developer with 1 year of experience under my belt. I
+        specialize crafting dynamic and accessible web applications that users love. Collaborating
+        seamlessly with design teams and backend developers, I've honed my craft in making visual
+        ideas a tangible reality.
+      </motion.p>
+    </>
+  );
+};
+
+export default About;
